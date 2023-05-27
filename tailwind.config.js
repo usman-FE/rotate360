cbe24adf07359957b966da56b5c10cea914713a3
Muhammad Usman <ch.usman564@gmail.com>
2023-05-27 14:41:36 +0500

    Basic rotation feature

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {},
+  plugins: [],
+};
+
+/*
+
+bodyOverflow: function (modal) {
+  document.querySelector('body').classList.remove('overflow-hidden', 'overflow-visible');
+  document.querySelector('body').classList.add(modal ? 'overflow-hidden' : 'overflow-visible');
+}
+ff0020
+*/
